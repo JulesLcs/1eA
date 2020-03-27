342f11a4b61336e00103de7e4d98671b179f0fa6
JulesLcs <jules.lucas.49000@gmail.com>
2020-03-27 17:21:16 +0100

    Tp4

---
+++ TP4/main.js
@@ -0,0 +1,7 @@
+function main(){
+    sumTest();
+    longestWordTest();
+    rangeTest();
+    occuTest();
+}
+main();
---
+++ TP4/tab.js
@@ -0,0 +1,37 @@
+function sum(numbers){
+    let s = 0;
+//    for(let i = 0; i < numbers.length;i++){
+//        s = s + numbers[i];
+//    }
+    for(let number of numbers){
+        s = s + number;
+   }
+    return s;
+}
+function longestWord(words){
+    let lw ="";
+    for(let i = 0; i < words.length; i++){
+        if(words[i].length > lw.length){
+            lw = words[i];
+        }
+    }
+    return lw;
+}
+function range(min, max, step){
+    let t = [];
+        while(min <= max){
+            t.push(min);
+            min = min + step;
+        }
+    return t;
+}
+function nbOccurences(wordList, word){
+    let occ = 0;
+    let wordsList = wordList.split(" ");
+    for(let i = 0; i < wordsList.length;i++){
+        if(wordsList[i] === word){
+            occ= occ + 1;
+        }
+    }
+    return occ;
+}
